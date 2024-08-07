e63d2b35bc55a6eff50634fc3a97e14f5f90811b
Vivekanand Mendhe <vivekanand.mendhe@infoorigin.com>
2024-08-07 12:38:08 +0530

    I added some components like header, login, register, activate-account, also contacts compoents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './pages/contacts/about/about.component';
+import { ServicesComponent } from './pages/contacts/services/services.component';
+import { ContactComponent } from './pages/contacts/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [{path: '', component:HomeComponent}];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+];
